bb213fdf2c779f5a7aff29fb543ab3da08b6262e
hoangnam1909 <nguyenhoangn023@gmail.com>
2022-08-07 22:53:13 +0700

    first commit

---
+++ JobSearch-SpringBoot/src/main/java/com/nhn/JobSearchSpringBoot/JobSearchSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.JobSearchSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchSpringBootApplication.class, args);
+	}
+
+}
---
+++ JobSearch-SpringBoot/src/test/java/com/nhn/JobSearchSpringBoot/JobSearchSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.JobSearchSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
