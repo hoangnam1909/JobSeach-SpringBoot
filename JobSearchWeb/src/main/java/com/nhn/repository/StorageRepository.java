84ecffd0a9fbdceaa7c8bcc67fb5f3ba638836b4
hoangnam1909 <nguyenhoangn023@gmail.com>
2022-10-01 16:03:48 +0700

    remove upload

--- JobSearchWeb/src/main/java/com/nhn/repository/StorageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nhn.repository;
-
-import com.nhn.model.ImageData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface StorageRepository extends JpaRepository<ImageData, String> {
-}
